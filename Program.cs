485480327ee1f48a8dee33b1ff7c68d2da963822
HUSNAIN <ranasunain584@gmail.com>
2021-11-16 09:22:47 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+ 
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow world");
+        }
+    }
+}
